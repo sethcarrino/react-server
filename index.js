1e80e61fdf48c61574c914d09bd6d66dea19577f
sethcarrino <sethcarrino@gmail.com>
2017-10-29 21:10:44 -0400

    added google oAuth with passport js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+	}, 
+	(accessToken, refreshToken, profile, done) => {
+		console.log('accessToken', accessToken);
+		console.log('refreshToken', refreshToken);
+		console.log('profile', profile);
+	})
+);
+
+app.get(
+	'/auth/google', passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 // writing a variable for handling heroku port environment. 
 // setup || to listen if environment is not avaible and for development on separate port
